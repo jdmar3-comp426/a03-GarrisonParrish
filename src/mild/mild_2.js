c844fb0915650b33bc0484c1d1a64f032fe507ff
Garrison Parrish <tpgarris@live.unc.edu>
2021-11-14 17:19:05 -0500

    nondestructive

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -68,7 +68,9 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   
+   let objectCopy = object;
+   delete objectCopy[key];
+   return objectCopy;
 }
 
 /**
