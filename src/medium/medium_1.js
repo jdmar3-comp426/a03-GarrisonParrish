446cabb3f650b660c10f9dfbff98971f0582a04f
Garrison Parrish <tpgarris@live.unc.edu>
2021-11-18 22:23:47 -0500

    parenthesis

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -28,7 +28,7 @@ export function getMedian(array) {
     let midpoint = Math.floor(array.length / 2);
     if (array.length % 2 == 0) {
         // even length => midpoint = mean of middle two indices
-        median = array[midpoint] + array[midpoint+1]) / 2;
+        median = (array[midpoint] + array[midpoint+1]) / 2;
     } else {
         // odd length => midpoint = middle value
         median = array[midpoint];
