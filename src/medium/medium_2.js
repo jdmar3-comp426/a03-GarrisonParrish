9626d15b7eb1ebcb761624ae2e2a55c1e80a7066
Garrison Parrish <tpgarris@live.unc.edu>
2021-11-26 18:30:37 -0500

    poop

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -96,7 +96,7 @@ export function getMakerHybrids(mpg_data) {
             let new_flag = false;
             for (let i = 0; i < hybrids_arr.length; i++) {
                 // check if the car's make matches a hybrid in the list
-                if (hybrid["make"] == car["make"]) {
+                if (hybrids_arr[i]["make"] == car["make"]) {
                     hybrids_arr[index]["hybrids"].push(car["id"]);  // add the car's id to the hybrids array
                     new_flag = true;
                 }
